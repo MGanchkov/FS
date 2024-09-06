14ede146561374d1f13409609bbe779b33cf9a18
MGanchkov <56094035+MGanchkov@users.noreply.github.com>
2024-09-06 23:01:21 +0300

    base interface for ViewModel

---
+++ FS.Core/ViewModels/IEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS.ViewModels;
+
+public interface IEntry
+{
+
+    /// <summary>
+    /// Уникальный идентификатор
+    /// </summary>
+    public long ID { get; set; }
+
+}
