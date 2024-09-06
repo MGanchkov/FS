0629ac799896467ffa1c6ceb55af1e622f995be9
MGanchkov <56094035+MGanchkov@users.noreply.github.com>
2024-09-06 22:35:23 +0300

    Добавьте файлы проекта.

---
+++ FS.Core/Models/CAccaunt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS.Models;
+
+public class CAccaunt : IEntry
+{
+    #region ctor
+
+    #endregion
+
+    #region Property
+
+
+    public long ID { get; set; }
+
+    /// <summary>
+    /// Отображаемое имя
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Логин подключения
+    /// </summary>    
+    public string Login { get; set; }
+
+    /// <summary>
+    /// Пароль для подключения
+    /// </summary>
+    public string Password { get; set; }
+
+    #endregion
+
+}
