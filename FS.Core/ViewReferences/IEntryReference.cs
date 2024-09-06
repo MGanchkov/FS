c57f022a9887af972e57af3bae31b2a3c038e6b8
MGanchkov <56094035+MGanchkov@users.noreply.github.com>
2024-09-06 23:11:11 +0300

    add interface ViewReference

---
+++ FS.Core/ViewReferences/IEntryReference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS.ViewReferences;
+
+public interface IEntryReference <TValue> where TValue : ViewModels.IEntry
+{
+    
+    public long ID { get; set; }
+
+    public TValue Loading();
+
+}
