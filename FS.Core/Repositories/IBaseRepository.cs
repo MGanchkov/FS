fbb41ed53349d45836b806025c14b2995df88cb1
MGanchkov <56094035+MGanchkov@users.noreply.github.com>
2024-09-06 22:55:35 +0300

    Добавлен базовый базовый интерфейс для доступа к базам данных

---
+++ FS.Core/Repositories/IBaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using FS.Models;
+
+namespace FS.Repositories;
+
+/// <summary>
+/// Базовый интерфейс доступа к базе данных
+/// </summary>
+/// <typeparam name="TValue"></typeparam>
+public interface IBaseRepository<TValue> where TValue : IEntry
+{
+
+    #region CRUD
+
+    /// <summary>
+    /// Добавить объект в базу данных
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public long Insert(TValue item);
+    /// <summary>
+    /// Вернуть объект из базы данных
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Если объект не существуует, то возращает <see cref="Nullable"/></returns>
+    public TValue? Item(long id);
+    /// <summary>
+    /// Обновляет объект в базе данных
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public bool Update(TValue item);
+    /// <summary>
+    /// Удалить объект из базы данных
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public bool Delete(long id);
+
+    #endregion
+
+    /// <summary>
+    /// Все объекты
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<TValue> GetAll();
+
+    /// <summary>
+    /// Поиск объектов
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public IEnumerable<TValue> Find(Func<TValue, bool> predicate) => GetAll().Where(predicate);
+
+
+}
