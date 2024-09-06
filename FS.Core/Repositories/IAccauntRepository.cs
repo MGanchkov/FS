d7122ad6193aab86bc832bb0f0966f8bbb3d86c8
MGanchkov <56094035+MGanchkov@users.noreply.github.com>
2024-09-06 22:58:19 +0300

    add interface IAccauntRepository

---
+++ FS.Core/Repositories/IAccauntRepository.cs
@@ -0,0 +1,17 @@
+﻿using FS.Models;
+
+namespace FS.Repositories;
+
+public interface IAccauntRepository : IBaseRepository<CAccaunt>
+{
+
+    /// <summary>
+    /// Создание нового аккаунта
+    /// </summary>
+    /// <param name="login">Логин создаваемого аккаунта (должен быть уникальным)</param>
+    /// <param name="password">Пароль создаваемого аккаунта</param>
+    /// <param name="name">Отображаемое имя</param>
+    /// <returns>Если создание не удалось, то вернёт <see cref="Nullable"/></returns>
+    public CAccaunt? Create(string login, string password, string? name = null);
+
+}
