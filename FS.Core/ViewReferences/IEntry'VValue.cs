93327ec10365be8a9891d820d021c154094ec398
MGanchkov <56094035+MGanchkov@users.noreply.github.com>
2024-09-06 23:33:19 +0300

    rename interface object Reference IEntry<VValue>

---
+++ FS.Core/ViewReferences/IEntry'VValue.cs
@@ -0,0 +1,27 @@
+﻿using VEntry = FS.ViewModels.IEntry;
+
+namespace FS.ViewReferences;
+
+/// <summary>
+/// Объект-ссылка
+/// </summary>
+/// <typeparam name="VValue">Тип из ViewModels</typeparam>
+public interface IEntry<VValue> where VValue : VEntry
+{
+    /// <summary>
+    /// Идентификатор объекта
+    /// </summary>
+    public long ID { get; }
+
+    /// <summary>
+    /// Является ли объект <see cref="Nullable"/>
+    /// </summary>
+    public bool IsNull { get; }
+
+    /// <summary>
+    /// Загрузка и возрат полной копии объекта для отображения.
+    /// </summary>
+    /// <returns></returns>
+    public VValue Loading();
+
+}
